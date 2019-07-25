8bc6362b631b7f4b1bc570682aaf71b03388ab08
aleksandrpnshkn <aleksandrpnshkn@gmail.com>
2019-07-25 21:22:39 +0400

    Updates

---
+++ precache-manifest.c21a8af267875da3638fea8045cd2234.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce30a4180f2188efa1880d35df3686a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa45d53668210311af3e",
+    "url": "/static/js/2.fb3e3218.chunk.js"
+  },
+  {
+    "revision": "d2498fa2b9808504e4ba",
+    "url": "/static/js/main.77a32e3a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
